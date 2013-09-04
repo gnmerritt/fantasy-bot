aeb68049b8dc3bc0d5d5945c23b9868c19bdded7
Nathan Merritt <gnm@the-merritts.net>
2013-09-04 18:29:00 -0400

    added dev config file

---
+++ config.js
@@ -0,0 +1,5 @@
+/* example config file */
+KEY="e92565d1-ba37-413a-b86a-e417062830f3"
+HOST="localhost:8000/d"
+PREFIX="/api/v1/"
+ROSTER=["RB","RB","WR","TE","WR","QB","WR","DST","RB","TE","QB","WR","RB"]
