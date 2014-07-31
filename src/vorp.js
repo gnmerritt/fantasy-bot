a6a7b6781aa79b397711b98b052117934e27b339
Nathan Merritt <gnm@the-merritts.net>
2014-07-30 20:22:21 -0400

    initial vorp commit

---
+++ src/vorp.js
@@ -0,0 +1,15 @@
+/**
+ * Calculates value-over-replacement for players by position
+ *
+ * Inputs:
+ *   list of player names & estimated points
+ *   object specifying position slots per fantasy team roster
+ *   number of fantasy teams in the league
+ */
+(function(document, window, undefined) {
+
+window.Vorp = function(pointEstimates, draftRoster, draftTeams) {
+
+};
+
+})(document, window);
