f2d53d482176c32a1dbda9cb41d25386ff70bb3d
Nathan Merritt <gnm@the-merritts.net>
2014-07-30 22:48:46 -0400

    finished up VORP calculations

--- src/vorp.js
+++ src/vorp.js
@@ -6,20 +6,40 @@
  *   position slots per fantasy team roster
  *   number of fantasy teams in the league
  * Output:
- *
+ *   Decorated input object with VORP added to each player
  */
 (function(document, window, undefined) {
 
 window.vorp = function(pointEstimates, draftRoster, draftTeams) {
     var POSITIONS = ["QB", "RB", "WR", "TE", "K", "DST"]
 
-    , replacementValues // map position -> replacement points
-
-    , getReplacementValue = function(position) {
+    /**
+     * Calculates replacement value by estimating demand for each
+     * position, and then averaging the projected value of the next
+     * 10% of available players at the position
+     */
+    , getReplacementValue = function(position, inputEstimates) {
         var teamDemand = getTeamDemand(position)
         , totalDemand = teamDemand * draftTeams
+
+        , players = inputEstimates[position] || []
+        , replacementStart = Math.min(Math.ceil(totalDemand),
+                                      players.length - 1)
+        , replacementRange = players.length * 0.1
+        , replacementStop = replacementStart + replacementRange
+        , replacementPlayers = players.slice(replacementStart, replacementStop)
+
+        , replacementValue = averagePoints(replacementPlayers)
         ;
-        console.log(position + " total: " + totalDemand + " team: " + teamDemand);
+        return replacementValue;
+    }
+
+    , averagePoints = function(players) {
+        var total = 0;
+        $.each(players, function(_, player) {
+            total += player.points;
+        });
+        return total / Math.max(players.length, 1);
     }
 
     /**
@@ -44,17 +64,58 @@ window.vorp = function(pointEstimates, draftRoster, draftTeams) {
         return demand;
     }
 
-    , calculateReplacements = function() {
-        replacementValues = {};
+    /**
+     * Returns a map of position -> replacement value
+     */
+    , calculateReplacementValues = function(inputEstimates) {
+        var replacementValues = {};
         $.each(POSITIONS, function(_, pos) {
-            replacementValues[pos] = getReplacementValue(pos);
+            replacementValues[pos] = getReplacementValue(pos, inputEstimates);
+        });
+        return replacementValues
+    }
+
+    /**
+     * Call the action function for every player
+     */
+    , forEveryPlayer = function(input, action) {
+        $.each(input, function(pos, playerList) {
+            $.each(playerList, function(i, player) {
+                action(player);
+            });
         });
     }
+
+    , cleanInput = function(input) {
+        var estimates = JSON.parse(JSON.stringify(input)) // :-P
+        ;
+        // convert player points to a float
+        forEveryPlayer(estimates, function(player) {
+            var pointsStr = player.points
+            pointsFloat = parseFloat(pointsStr)
+            ;
+            player.points = pointsFloat;
+        });
+        // now sort all the lists by point total, descending
+        $.each(estimates, function(position, playerList) {
+            playerList.sort(function(a,b) { return b.points - a.points; });
+        });
+        return estimates;
+    }
+
+    //
+    // Now do the real work. Clean the input, calculate replacement values
+    // and return a decorated version of the input object with vorb
+    //
+    , inputEstimates = cleanInput(pointEstimates)
+    , replacementValues = calculateReplacementValues(inputEstimates)
     ;
 
-    calculateReplacements();
+    forEveryPlayer(inputEstimates, function(player) {
+        player.vorb = player.points - replacementValues[player.pos];
+    });
 
-    return replacementValues;
+    return inputEstimates;
 };
 
 })(document, window);
