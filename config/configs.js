eb7e80ec51f4a332430a2b9096ab013f29ae9633
Nathan Merritt <gnm@the-merritts.net>
2014-09-17 17:54:09 -0400

    consolidated configs file a bit

--- config/configs.js
+++ config/configs.js
@@ -1,40 +1,46 @@
-/* example config file */
+/**
+ * Draft configuration profiles
+ */
+(function() {
+
+var base = {
+    HOST: "draft.gnmerritt.net"
+    , PREFIX: "/api/v1/"
+};
+
+var manual = {
+    KEY: "", HOST: "", PREFIX: ""
+    , MANUAL: true
+};
+
 CONFIG["DEBUG"] = {
     KEY: "8891a052-7d14-48db-9c2c-c0a59f87e927"
-    , HOST:"localhost:8000/d"
-    , PREFIX: "/api/v1/"
+    , HOST:"localhost:8000"
+    , PREFIX: "/d/api/v1/"
 };
 
-CONFIG["AUTOBOTS-2013"] = {
+CONFIG["AUTOBOTS-2013"] = $.extend({}, base, {
     KEY: "8891a052-7d14-48db-9c2c-c0a59f87e927"
-    , HOST: "draft.gnmerritt.net"
-    , PREFIX: "/api/v1/"
-};
+});
 
-CONFIG["AUTOBOTS-2014"] = {
+CONFIG["AUTOBOTS-2014"] = $.extend({}, base, {
     KEY: "36164d39-014c-484f-b9f7-70fa07bcf6e4"
-    , HOST: "draft.gnmerritt.net"
-    , PREFIX: "/api/v1/"
-};
+});
 
-CONFIG["FORGET-2014"] = {
+CONFIG["FORGET-2014"] = $.extend({}, base, {
     KEY: "70041038-ecb3-459d-b388-2df91a1e8057"
-    , HOST: "draft.gnmerritt.net"
-    , PREFIX: "/api/v1/"
-};
+});
 
-CONFIG["YAHOO-MOCK"] = {
-    KEY: "", HOST: "", PREFIX: ""
-    , MANUAL: true
-    , ROSTER: "QB, RB, RB, WR, WR, WR, TE, K, DST, BN, BN, BN, BN, BN, BN"
+CONFIG["YAHOO-MOCK"] = $.extend({}, manual, {
+    ROSTER: "QB, RB, RB, WR, WR, WR, TE, K, DST, BN, BN, BN, BN, BN, BN"
     , ROSTER_SLOTS: 15
     , TEAMS: 10
-};
+});
 
-CONFIG["CLOWN"] = {
-    KEY: "", HOST: "", PREFIX: ""
-    , MANUAL: true
-    , ROSTER: "QB, RB, RB, WR, WR, TE, WR/RB, K, DST, BN, BN, BN, BN"
+CONFIG["CLOWN"] = $.extend({}, manual, {
+    ROSTER: "QB, RB, RB, WR, WR, TE, WR/RB, K, DST, BN, BN, BN, BN"
     , ROSTER_SLOTS: 13
     , TEAMS: 12
-};
+});
+
+})();
